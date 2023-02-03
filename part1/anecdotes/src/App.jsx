3778be4bb68b91f85bfcb1e4a61e2696c9ab8239
razlevio <razlevio.55@gmail.com>
2023-02-03 18:19:37 +0200

    updating the styling of the page

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,10 +1,10 @@
 import { useState } from 'react'
 
 function App() {
-
   return (
-    <div className="flex flex-col gap-5 justify-center items-center">
-      <h1>Hello World</h1>
+    <div className="flex flex-col gap-2 justify-center items-center">
+      <h1 className="text-4xl p-5">Hello World</h1>
+      <button className="text-pink-400 border border-pink-400 hover:bg-pink-400 hover:text-white active:bg-pink-600 font-bold uppercase px-8 py-2 w-54 rounded outline-none focus:outline-none ease-linear transition-all duration-150">Generate Anecdote</button>
     </div>
   )
 }
