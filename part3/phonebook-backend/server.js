a4d3a7a6d2b8d2a8170a8308e30bdd6c0946a0a0
razlevio <razlevio.55@gmail.com>
2023-02-19 19:18:29 +0200

    intial routes

--- part3/phonebook-backend/server.js
+++ part3/phonebook-backend/server.js
@@ -1,13 +1,10 @@
-import express from "express";
-import cors from "cors";
-import bodyParser from "body-parser";
+import express from "express"
 
 const server = express();
-server.use(cors());
 server.use(express.json());
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
-let notes = [
+let phonebook = [
     { 
       "id": 1,
       "name": "Arto Hellas", 
@@ -30,6 +27,30 @@ let notes = [
     }
 ]
 
+server.get("/", (req, res) => res.send("<h1>Phonebook Server</h1>"))
+
 server.get("/api/persons", (req, res) => {
-    res.json(notes);
+    res.json(phonebook)
 });
+
+server.get("/info", (req, res) => {
+    const date = new Date().toUTCString()
+    const numOfPeople = phonebook.length;
+    res.send(`<div><h2>Phonebook has info for ${numOfPeople} people</h2><h3>${date}</h3></div>`)
+});
+
+server.get("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const data = phonebook.filter(pers => pers.id === id)
+    if(data.length === 0) res.status(404).end()
+    else res.json(data);
+});
+
+server.delete("/api/persons/del/:id", (req, res) => {
+    const id = Number(req.params.id);
+    phonebook = phonebook.filter(pers => pers.id !== id);
+    res.status(204).end()
+});
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
+
